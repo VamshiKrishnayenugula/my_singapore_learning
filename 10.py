3185d7a93761d8d27c6767e17bc2092e52e06c34
vamshi <vamshikrishnayenugula@gmail.com>
2024-02-10 00:21:07 +0800

    10.py file

---
+++ 10.py
@@ -0,0 +1,10 @@
+#absolute value of a number
+"""Let’s say you have a negative number and you want to return 
+the absolute value of that number; you can use the abs()
+function. The Python abs() function is used to return the 
+absolute value of any number (positive, negative, and complex 
+numbers)."""
+
+list1 = [-12, -45, -67, -89, -34, 67, -13]
+
+print([abs(i) for i in list1])
